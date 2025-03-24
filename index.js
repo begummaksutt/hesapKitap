795d5023c213eb29a736a3bddc74216372f68e37
begummaksutt <begummaksutt@gmail.com>
2025-03-24 13:40:49 +0300

    ilk sayfa oluşturuldu

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-/**
- * @format
- */
+//import libraries
+import React from "react";
+import { Text , Appregisrty }from "react-native";
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+//create component
+const App = () => {
+    return <Text> Merhaba Hoşgeldiniz</Text>;
+};
 
-AppRegistry.registerComponent(appName, () => App);
+//render component to screen
+Appregisrty.registerComponent("HESAPKİTAP",  ()  => App );
